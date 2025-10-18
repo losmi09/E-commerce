2303221579b239c9827190ed2c340aca374770b0
losmi09 <radic3000@gmail.com>
2025-10-18 21:31:58 +0200

    Added a file which filter and sorts data from database

---
+++ utils/filtering-sorting.js
@@ -0,0 +1,28 @@
+const filterAndSort = (reqQuery, defaultSort) => {
+  const { page = 1, limit = 100 } = reqQuery;
+
+  const skip = (+page - 1) * +limit;
+
+  const excludeFromQuery = ['page', 'limit', 'sort'];
+
+  const query = structuredClone(reqQuery);
+
+  if (!query.sort) query.sort = defaultSort;
+
+  const sortingBy = query.sort.split(',');
+
+  const sorting = Array.from({ length: sortingBy.length }, () => {
+    return {};
+  });
+
+  query.sort.split(',').forEach((el, i) => {
+    if (el.includes('-')) sorting[i][el.slice(1)] = 'desc';
+    else sorting[i][el] = 'asc';
+  });
+
+  excludeFromQuery.forEach(el => delete query[el]);
+
+  return { skip, limit, query, sorting };
+};
+
+export default filterAndSort;
