c10c795f1f522f40cfb8e15220b1bc5c8e0d214d
losmi09 <radic3000@gmail.com>
2025-10-19 14:26:32 +0200

    Added a function for deleting category in category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,3 +4,4 @@ export const getAllCategories = factory.getAll('category', 'id');
 export const getCategory = factory.getOne('category');
 export const createCategory = factory.createOne('category');
 export const updateCategory = factory.updateOne('category');
+export const deleteCategory = factory.deleteOne('category');
