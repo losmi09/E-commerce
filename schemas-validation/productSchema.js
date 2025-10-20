1f7bfd44785c74a1f37d8888262be1be15090b05
losmi09 <radic3000@gmail.com>
2025-10-20 22:12:26 +0200

    feat: add a categoryId in schema

--- schemas-validation/productSchema.js
+++ schemas-validation/productSchema.js
@@ -4,6 +4,7 @@ const productSchema = Joi.object({
   name: Joi.string().required().trim(),
   description: Joi.string().required().trim(),
   price: Joi.number().positive().required(),
+  categoryId: Joi.number().integer().positive().required(),
 });
 
 export default productSchema;
