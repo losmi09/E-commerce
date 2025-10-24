742a5a6da06363e0f53b1723aecbb80d456aa809
losmi09 <radic3000@gmail.com>
2025-10-24 20:58:40 +0200

    feat: handle unhandled exceptions & rejections

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 import { PrismaClient } from '@prisma/client';
 import app from './app.js';
 
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION 💥');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const prisma = new PrismaClient();
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}...`));
+const server = app.listen(port, () =>
+  console.log(`Server running on port ${port}...`)
+);
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION 💥');
+  console.log(err.name, err.message);
+  server.close(() => process.exit(1));
+});
 
 export default prisma;
