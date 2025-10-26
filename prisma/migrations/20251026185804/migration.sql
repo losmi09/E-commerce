4a4c8b4b5976fbec8f6844674945c96cda41139d
losmi09 <radic3000@gmail.com>
2025-10-26 20:00:49 +0100

    add image in Product model

---
+++ prisma/migrations/20251026185804/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `photo` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "photo",
+ADD COLUMN     "avatar" TEXT NOT NULL DEFAULT 'default.jpg';
---
+++ prisma/migrations/20251026185916/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatar` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "avatar",
+ADD COLUMN     "photo" TEXT NOT NULL DEFAULT 'default.jpg';
