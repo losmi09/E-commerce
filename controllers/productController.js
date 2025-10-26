8c8a0343e73527e45a13c42926aebf0a8d99c334
losmi09 <radic3000@gmail.com>
2025-10-27 00:31:17 +0100

    feat: add upload image functionality for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,36 @@
+import { extname } from 'path';
+import multer from 'multer';
+import sharp from 'sharp';
 import * as factory from './handlerFactory.js';
+import { fileFilter } from './userController.js';
+import catchAsync from '../utils/catchAsync.js';
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+export const uploadProductImage = upload.single('image');
+
+export const resizeProductImage = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  const ext = extname(req.file.originalname);
+
+  req.fileName = `product-${req.body.name}-${Date.now()}${ext}`;
+
+  await sharp(req.file.buffer)
+    .resize(1000, 1000)
+    .toFormat('jpg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/products/${req.fileName}`);
+
+  req.body.image = req.fileName;
+
+  next();
+});
 
 export const getAllProducts = factory.getAll('product', 'price');
 export const getProduct = factory.getOne('product');
