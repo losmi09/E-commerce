eba94fe8d54b427ee40be49d66a479b61352dad8
losmi09 <radic3000@gmail.com>
2025-10-27 00:32:21 +0100

    feat: upload an image before creating a product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,6 +14,8 @@ router
   .post(
     authController.protect,
     authController.restrictTo('admin'),
+    productController.uploadProductImage,
+    productController.resizeProductImage,
     productController.createProduct
   );
 
