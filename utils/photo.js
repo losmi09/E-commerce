1432296dae7f3cae0288b1e01f54e72676b75d7e
losmi09 <radic3000@gmail.com>
2025-10-29 00:11:06 +0100

    feat: add function to check if user can upload a photo

--- utils/photo.js
+++ utils/photo.js
@@ -4,14 +4,22 @@ import { sanitizeOutput } from '../controllers/authController.js';
 import deleteImage from './deleteImage.js';
 import prisma from '../server.js';
 
-const photo = change =>
+const findUser = catchAsync(async (req, next) => {
+  let where = { id: +req.user.id };
+
+  if (req.user.role === 'admin') where = { id: +req.params.id || +req.user.id };
+
+  const user = await prisma.user.findUnique({
+    where,
+  });
+
+  if (!user) return next(new AppError('No user found with that ID', 404));
+
+  return user;
+});
+
+export const checkBeforeUpload = change =>
   catchAsync(async (req, res, next) => {
-    console.log(req.file.filename);
-    let where = { id: +req.user.id };
-
-    if (req.user.role === 'admin')
-      where = { id: +req.params.id || +req.user.id };
-
     if (req.params.id !== 'me' && req.user.role !== 'admin')
       return next(
         new AppError(
@@ -22,21 +30,24 @@ const photo = change =>
         )
       );
 
+    await findUser(req, next);
+
+    next();
+  });
+
+const photo = change =>
+  catchAsync(async (req, res, next) => {
     if (change === 'add' && !req.file)
       return next(new AppError('Please upload an image', 400));
 
     const imageName = change === 'add' ? req.file.filename : 'default.jpg';
 
-    let user = await prisma.user.findUnique({
-      where,
-    });
-
-    if (!user) return next(new AppError('No user found with that ID', 404));
+    let user = await findUser(req);
 
     if (user.photo !== 'default.jpg') deleteImage('users', user.photo);
 
     user = await prisma.user.update({
-      where,
+      where: { id: user.id },
       data: { photo: imageName },
     });
 
