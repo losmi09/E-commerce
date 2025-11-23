2c9ead94622112d2ee33a941aedc56820745384d
losmi09 <radic3000@gmail.com>
2025-11-23 05:18:01 +0100

    refactor: remove unnecessary argument from getAll function

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,6 @@
 import * as factory from './handlerFactory.js';
 
-export const getCartItems = factory.getAll('cartItem', 'cartId');
+export const getCartItems = factory.getAll('cartItem');
 export const getCartItem = factory.getOne('cartItem');
 export const addToCart = factory.createOne('cartItem');
 export const removeFromCart = factory.deleteOne('cartItem');
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,6 @@
 import * as factory from './handlerFactory.js';
 
-export const getAllReviews = factory.getAll('review', 'rating');
+export const getAllReviews = factory.getAll('review');
 export const getReview = factory.getOne('review');
 export const createReview = factory.createOne('review');
 export const updateReview = factory.updateOne('review');
