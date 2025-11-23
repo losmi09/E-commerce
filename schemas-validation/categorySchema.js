c4bb6e10352f0bbaa63935fc42fd9f52082cbf22
losmi09 <radic3000@gmail.com>
2025-11-23 03:52:52 +0100

    feat: add image field in category schema

--- schemas-validation/categorySchema.js
+++ schemas-validation/categorySchema.js
@@ -3,6 +3,7 @@ import Joi from 'joi';
 const categorySchema = Joi.object({
   name: Joi.string().required().trim(),
   description: Joi.string().required().trim(),
+  image: Joi.string(),
 });
 
 export default categorySchema;
