95440da173ecfd369f7cb96e944a9890cf2416f5
losmi09 <radic3000@gmail.com>
2025-11-23 03:02:57 +0100

    docs: add a comment to increase clarity

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,5 @@
 const catchAsync = fn => {
-  return (req, res, next) => fn(req, res, next).catch(next);
+  return (req, res, next) => fn(req, res, next).catch(next); // catch(next) passes error to globalErrorHandler
 };
 
 export default catchAsync;
